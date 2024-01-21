ffa1748cefc12a5499c9747d47f169e508c4fa6a
Joshua Kim <jckim014@gmail.com>
2024-01-20 16:33:37 -0800

    Implement insertion sort

--- main.py
+++ main.py
@@ -120,6 +120,26 @@ def bubble_sort(draw_info, ascending = True):
     return lst
 
 
+def insertion_sort(draw_info, ascending=True):
+    lst = draw_info.lst
+
+    for i in range(1, len(lst)):
+        current = lst[i]
+        while True:
+            ascending_sort = i > 0 and lst[i - 1] > current and ascending
+            descending_sort = i > 0 and lst[i - 1] < current and not ascending
+
+            if not ascending_sort and not descending_sort:
+                break
+            lst[i] = lst[i - 1]
+            i = i - 1
+            lst[i] = current
+            draw_list(draw_info, {i: draw_info.GREEN, i - 1: draw_info.RED}, True)
+            yield True
+
+    return lst
+
+
 def generate_starting_list(n, min_val, max_val):
     lst = []
 
@@ -180,7 +200,12 @@ def main():
                 ascending = True
             elif event.key == pygame.K_d and not sorting:
                 ascending = False
-
+            elif event.key == pygame.K_i and not sorting:
+                sorting_algo = insertion_sort
+                sorting_algo_name = "Insertion Sort"
+            elif event.key == pygame.K_b and not sorting:
+                sorting_algo = bubble_sort
+                sorting_algo_name = "Bubble Sort"
 # *** display some sort of time taken for efficiency comparison
 # *** allow user input of parameters
     pygame.quit()
